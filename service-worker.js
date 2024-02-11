385a72feb45cb7ead9c6d445ca1631b68c7dd4d8
hortifrutibrasileiros <91627384+hortifrutibrasileiros@users.noreply.github.com>
2024-02-11 01:18:12 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -38,17 +38,16 @@ self.addEventListener('fetch', (event) => {
 });
 
 self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.filter(function(cacheName) {
-                    return true;
-                }).map(function(cacheName) {
-                    return caches.delete(cacheName);
-                })
-            );
-        })
-    );
+  event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+          return Promise.all(
+              cacheNames.filter(function(cacheName) {
+                  return true;
+              }).map(function(cacheName) {
+                  return caches.delete(cacheName);
+              })
+          );
+      })
+  );
 });
-
 
